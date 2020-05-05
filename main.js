2d3d7230486ecfb8be665566e4e283c6225afb55
rob <52644668+robyoung75@users.noreply.github.com>
2020-05-05 10:16:04 -0600

    get and set methods for courses

--- main.js
+++ main.js
@@ -5,9 +5,29 @@ const menu = { //menu object
         appetizers: [], // property or key of _courses object.
         mains: [],
         desserts: [],
-    }
+    },
+    get appetizers() {
+        return this._courses.appetizers;
+    },
+    get mains() {
+        return this._courses.mains;
+    },
+    get desserts() {
+        return this._courses.desserts;
+    },
+    set appetizers(appetizers) {
+        this._courses.appetizers = this.appetizers;
+    },
+    set mains(mains) {
+        this._courses.mains = this.mains;
+    },
+    set desserts(desserts) {
+        this._courses.appetizers = this.desserts;
+    },
+    
 };
 
 // console.log(menu); // logs { _courses: { appetizers: [], mains: [], desserts: [] } }
 // console.log(menu._courses); // logs { appetizers: [], mains: [], desserts: [] }
 // console.log(menu._courses.appetizers); // logs []
+
