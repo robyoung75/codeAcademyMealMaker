6b417e0d65be4428ea4c34bb140ede533bce7272
rob <52644668+robyoung75@users.noreply.github.com>
2020-05-05 09:59:46 -0600

    Menu object

---
+++ main.js
@@ -0,0 +1,13 @@
+// Code Academy Meal Maker
+
+const menu = { //menu object
+    _courses: { // _courses object, property of menu.
+        appetizers: [], // property or key of _courses object.
+        mains: [],
+        desserts: [],
+    }
+};
+
+// console.log(menu); // logs { _courses: { appetizers: [], mains: [], desserts: [] } }
+// console.log(menu._courses); // logs { appetizers: [], mains: [], desserts: [] }
+// console.log(menu._courses.appetizers); // logs []
